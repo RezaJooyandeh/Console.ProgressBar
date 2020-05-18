75eca874308edbfd56f032a5bbe1ef89679b8ae1
Reza Jooyandeh <mr.jooyandeh@gmail.com>
2020-05-18 10:11:43 -0700

    Initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,118 @@
+﻿using SimpleConsoleProgressBar;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+	internal class Program
+	{
+		private static async Task Main()
+		{
+			{
+				Console.WriteLine("Default");
+				var progressBar = new ProgressBar();
+				for (var i = 0; i <= 60; i++)
+				{
+					await Task.Delay(20);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Custom Length");
+				var progressBar = new ProgressBar(length: 60);
+				for (var i = 0; i <= 60; i++)
+				{
+					await Task.Delay(10);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Custom range");
+				var progressBar = new ProgressBar(minimum: 1, maximum: 20);
+				for (var i = 1; i <= 18; i += 1)
+				{
+					await Task.Delay(20);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Decimal range");
+				var progressBar = new ProgressBar(minimum: 0, maximum: 1m);
+				for (var i = 0m; i <= 0.9m; i += 0.1m)
+				{
+					await Task.Delay(20);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Custom symbols");
+				var progressBar = new ProgressBar(completedSymbol: "‡", incompletedSymbol: " ");
+				for (var i = 0; i <= 40; i++)
+				{
+					await Task.Delay(10);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Custom Format");
+				var progressBar = new ProgressBar(format: "|{0}| {1}% completed.");
+				for (var i = 0; i <= 89; i++)
+				{
+					await Task.Delay(10);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Custom Color");
+				var progressBar = new ProgressBar(format: "{0}", foregroundColor: ConsoleColor.Green, backgroundColor: ConsoleColor.Blue);
+				for (var i = 0; i <= 70; i++)
+				{
+					await Task.Delay(10);
+					progressBar.Update(i);
+				}
+				Console.WriteLine();
+			}
+
+			{
+				Console.WriteLine("Super custom");
+				ConsoleColor foreground = Console.ForegroundColor;
+				ConsoleColor background = Console.BackgroundColor;
+
+				for (var i = 0; i <= 70; i++)
+				{
+					await Task.Delay(10);
+
+					Console.CursorLeft = 10;
+					Console.CursorTop = 15;
+					Console.Write("[[[[");
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.BackgroundColor = ConsoleColor.DarkMagenta;
+					Console.Write(ProgressBar.Build(
+						value: i,
+						minimum: 0,
+						maximum: 100,
+						length: 50,
+						completedSymbol: "‡",
+						incompletedSymbol: "."));
+					Console.ForegroundColor = foreground;
+					Console.BackgroundColor = background;
+					Console.Write("]]] {0}%", i);
+				}
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ SimpleConsoleProgressBar/ProgressBar.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text;
+
+namespace SimpleConsoleProgressBar
+{
+	public class ProgressBar
+	{
+		private readonly decimal _minimum;
+		private readonly decimal _maximum = 100;
+		private readonly int _length = 80;
+		private readonly string _completedSymbol;
+		private readonly string _inCompletedSymbol;
+		private readonly string _format;
+		private readonly ConsoleColor? _foregroundColor;
+		private readonly ConsoleColor? _backgroundColor;
+		private Position? _positionToDraw;
+
+		public ProgressBar(
+			decimal minimum = 0,
+			decimal maximum = 100,
+			int length = 80,
+			string completedSymbol = "█",
+			string incompletedSymbol = "░",
+			string format = "[{0}] {1}",
+			ConsoleColor? foregroundColor = null,
+			ConsoleColor? backgroundColor = null,
+			Position? positionToDraw = null)
+		{
+			VerifyMinimumMaximum(minimum, maximum);
+			VerifyLengthValid(length);
+
+			_minimum = minimum;
+			_maximum = maximum;
+			_completedSymbol = completedSymbol;
+			_inCompletedSymbol = incompletedSymbol;
+			_length = length;
+			_positionToDraw = positionToDraw;
+			_format = format;
+			_foregroundColor = foregroundColor;
+			_backgroundColor = backgroundColor;
+		}
+
+		public void Update(decimal value)
+		{
+			VerifyValueInRange(value, _minimum, _maximum);
+
+			using (new CursorPosition(PositionToDraw()))
+			using (new ConsoleColors(_foregroundColor, _backgroundColor))
+				Console.WriteLine(_format, Build(value, _minimum, _maximum, _length, _completedSymbol, _inCompletedSymbol), value);
+
+			Position PositionToDraw()
+			{
+				if (_positionToDraw is null)
+					_positionToDraw = new Position(Console.CursorLeft, Console.CursorTop);
+
+				return _positionToDraw.Value;
+			}
+		}
+
+		public static string Build(decimal value, decimal minimum = 0, decimal maximum = 100, int length = 80, string completedSymbol = "█", string incompletedSymbol = "░")
+		{
+			VerifyMinimumMaximum(minimum, maximum);
+			VerifyValueInRange(value, minimum, maximum);
+			VerifyLengthValid(length);
+
+			if (maximum == minimum)
+				return string.Empty;
+
+			var progress = (int)((value - minimum) * length / (maximum - minimum));
+
+			var sb = new StringBuilder(length);
+
+			for (var i = 0; i < progress; i++)
+				sb.Append(completedSymbol);
+			for (var i = progress; i < length; i++)
+				sb.Append(incompletedSymbol);
+
+			return sb.ToString();
+		}
+
+		private static void VerifyLengthValid(int length)
+		{
+			if (length < 0)
+				throw new ArgumentOutOfRangeException(nameof(length), "Length should not be negative.");
+		}
+
+		private static void VerifyValueInRange(decimal value, decimal minimum, decimal maximum)
+		{
+			if (value > maximum || value < minimum)
+				throw new ArgumentOutOfRangeException(nameof(value), "Value should be between minimum and maximum.");
+		}
+
+		private static void VerifyMinimumMaximum(decimal minimum, decimal maximum)
+		{
+			if (minimum > maximum)
+				throw new ArgumentOutOfRangeException(nameof(minimum), "Minimum value should be smaller than maximum.");
+		}
+
+		private readonly struct CursorPosition : IDisposable
+		{
+			private readonly Position _position;
+
+			public CursorPosition(Position position)
+			{
+				_position = new Position(Console.CursorLeft, Console.CursorTop);
+
+				Console.CursorLeft = position.Left;
+				Console.CursorTop = position.Top;
+			}
+
+			public void Dispose()
+			{
+				Console.CursorLeft = _position.Left;
+				Console.CursorTop = _position.Top;
+			}
+		}
+
+		private readonly struct ConsoleColors : IDisposable
+		{
+			private readonly ConsoleColor _foregroundColor;
+			private readonly ConsoleColor _backgroundColor;
+
+			public ConsoleColors(ConsoleColor? foregroundColor, ConsoleColor? backgroundColor)
+			{
+				_foregroundColor = Console.ForegroundColor;
+				_backgroundColor = Console.BackgroundColor;
+
+				if (foregroundColor != null)
+					Console.ForegroundColor = foregroundColor.Value;
+				if (backgroundColor != null)
+					Console.BackgroundColor = backgroundColor.Value;
+			}
+
+			public void Dispose()
+			{
+				Console.ForegroundColor = _foregroundColor;
+				Console.BackgroundColor = _backgroundColor;
+			}
+		}
+
+		public readonly struct Position
+		{
+			public readonly int Left;
+			public readonly int Top;
+
+			public Position(int left, int top)
+			{
+				Left = left;
+				Top = top;
+			}
+		}
+	}
+}
